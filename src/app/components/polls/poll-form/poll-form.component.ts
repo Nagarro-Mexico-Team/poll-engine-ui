6ebc22063e388eba7aea09024c9942143fadbe3e
isidroleos <isidro.leos@nagarro.com>
2021-05-24 11:51:40 -0500

    Se agrega funcionalidad para guardar lo que se captura en el formulario de preguntas y se añaden algunas validaciones básicas.

--- src/app/components/polls/poll-form/poll-form.component.ts
+++ src/app/components/polls/poll-form/poll-form.component.ts
@@ -1,47 +1,52 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Poll } from '../../../models/poll';
-import { GridData } from '../../commons/grids/basic-grid/basic-grid.component';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import {Component, OnInit, Input} from '@angular/core';
+import {Poll} from '../../../models/poll';
+import {GridData} from '../../commons/grids/basic-grid/basic-grid.component';
+import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
-	selector: 'app-poll-form',
-	templateUrl: './poll-form.component.html',
-	styleUrls: ['./poll-form.component.css']
+  selector: 'app-poll-form',
+  templateUrl: './poll-form.component.html',
+  styleUrls: ['./poll-form.component.css']
 })
 export class PollFormComponent implements OnInit {
 
-	@Input() poll: Poll;
-	gridData: GridData;
-	showOp: boolean = false;
-	modal: any;
+  @Input() poll: Poll;
+  gridData: GridData;
+  showOp: boolean = false;
+  modal: any;
 
-	constructor(private modalService: NgbModal) {
-		this.gridData = {rows:[],fieldNames: []};
-	}
+  constructor(private modalService: NgbModal) {
+    this.gridData = {rows: [], fieldNames: []};
+  }
 
-	ngOnInit(): void {
-		this.prepareGridData();
-	}
+  ngOnInit(): void {
+    this.prepareGridData();
+  }
 
-	prepareGridData() {
-		
-		this.gridData.rows = this.poll.questions.map((question) => {
-			return {
-				"questionId": question.questionId,
-				"questionNumber": question.questionNumber,
-				"questionText": question.questionText,
-				"questionValue": question.questionValue,
-				"questionAnswer": question.questionAnswer,
-				"questionHint": question.questionHint
-			}
-		});
-		this.gridData.fieldNames = ["questionId", "questionNumber","questionText",
-				"questionValue","questionAnswer","questionHint"]
-	}
+  prepareGridData() {
 
-	addNewQuestionClick(content: any) {
-		console.log(content);
- 		this.modal = content;
- 		this.modalService.open(content, {centered: true, size: 'xl'});
-	}
+    this.gridData.rows = this.poll.questions.map((question) => {
+      return {
+        "questionId": question.questionId,
+        "questionNumber": question.questionNumber,
+        "questionText": question.questionText,
+        "questionValue": question.questionValue,
+        "questionAnswer": question.questionAnswer,
+        "questionHint": question.questionHint
+      }
+    });
+    this.gridData.fieldNames = ["questionId", "questionNumber", "questionText",
+      "questionValue", "questionAnswer", "questionHint"]
+  }
+
+  addNewQuestionClick(content: any) {
+    console.log(content);
+    this.modal = content;
+    this.modalService.open(content, {centered: true, size: 'xl'});
+  }
+
+  doOnSaveQuestion(item: any) {
+    console.log(item);
+    this.gridData.rows.push(item);
+  }
 }
--- src/app/components/polls/poll-form/question-form/question-form.component.ts
+++ src/app/components/polls/poll-form/question-form/question-form.component.ts
@@ -1,18 +1,39 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
+import {FormGroup, FormBuilder, Validators} from '@angular/forms';
 
 @Component({
-	selector: 'app-question-form',
-	templateUrl: './question-form.component.html',
-	styleUrls: ['./question-form.component.css']
+  selector: 'app-question-form',
+  templateUrl: './question-form.component.html',
+  styleUrls: ['./question-form.component.css']
 })
 export class QuestionFormComponent implements OnInit {
-	@Input() modal: any;
+  @Input() modal: any;
+  @Output() onSave: EventEmitter<any> = new EventEmitter<any>();
+  @Output() onSaveAndContinue: EventEmitter<any> = new EventEmitter<any>();
+  //@Output()    onCancel()
 
-	constructor() { }
+  questionForm: any;
 
-	ngOnInit(): void { }
-	
-	
+  constructor(private formBuilder: FormBuilder) {
 
+
+  }
+
+  ngOnInit(): void {
+    this.questionForm = this.formBuilder.group({
+      questionId: [-1],
+      questionNumber: [1, Validators.required],
+      questionText: ['', Validators.required],
+      questionValue: [0, Validators.required],
+      questionAnswer: ['', Validators.required],
+      questionHint: ['']
+    });
+  }
+
+
+  doSaveQuestionClick(event) {
+  	if (this.questionForm.valid) {
+	  	this.onSave.emit(this.questionForm.value)
+	}
+  }
 }
